6f0d6d3055d2c2b64ab4de6ed8cf2fc021d8e873
ars231106 <aryansurani32@gmail.com>
2026-07-15 16:20:36 +0300

    Time: 0 ms (100%), Space: 10.1 MB (38.32%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
+   /* vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans = {{}};
+        
         for(int i = 0; i<nums.size(); i++){
             int size = ans.size();
 
@@ -12,6 +13,24 @@ public:
             }
         }
 
+        return ans;  */
+
+//dfs - backtracking approach
+    vector<vector<int>> ans;
+    
+    void dfsBacktrack(vector<int>& nums, vector<int>& curr, int i){
+        ans.push_back(curr);
+
+        for(int j = i; j<nums.size(); j++){
+            curr.push_back(nums[j]);
+            dfsBacktrack(nums, curr, j + 1);
+            curr.pop_back();
+        }
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> curr;    
+        dfsBacktrack(nums, curr, 0);
         return ans;
     }
 };
