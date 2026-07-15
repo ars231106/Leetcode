1550d72cf5317fa71860dc641eae926332d51fa2
ars231106 <aryansurani32@gmail.com>
2026-07-15 08:33:56 +0300

    Time: 0 ms (100%), Space: 146.7 MB (77.9%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -14,27 +14,38 @@
 class Solution {
 public:
     int minDepth(TreeNode* root) {
-        if (root == NULL)
+        if(root == NULL){
             return 0;
-
-        queue<pair<TreeNode*, int>> q;
-        q.push({root, 1});
-
-        while (!q.empty()) {
-            TreeNode* node = q.front().first;
-            int depth = q.front().second;
-            q.pop();
-
-            if (node->left == NULL && node->right == NULL)
-                return depth;
-
-            if (node->left)
-                q.push({node->left, depth + 1});
-
-            if (node->right)
-                q.push({node->right, depth + 1});
         }
 
-        return 0;
-    }
+        queue<TreeNode*> q;
+        q.push(root);
+
+        int depth = 1;
+
+        while(!q.empty()){
+            int size = q.size();
+
+            while(size--){
+                TreeNode* node = q.front();
+                q.pop();
+                
+                if(node -> left == NULL && node -> right == NULL){
+                    return depth;
+                }
+                
+                if(node -> left){
+                    q.push(node -> left);
+                }
+                
+                if(node -> right){
+                    q.push(node -> right);
+                }
+            }
+
+            depth = depth + 1;
+        }
+        
+        return depth;
+      }
 };
