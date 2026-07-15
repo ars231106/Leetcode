4cc004b86394123810592535eb0100921a4c5f2a
ars231106 <aryansurani32@gmail.com>
2026-07-15 09:50:04 +0300

    Time: 14 ms (98.41%), Space: 23.6 MB (35.93%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -11,37 +11,18 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-
-        //CASE 1 : leftLCA = NULL and righLCA = NULL; return NULL
         if(root == NULL){
             return NULL;
         }
 
-        if(root == p || root == q){
-            return root;
+        if(p -> val < root -> val && q -> val < root -> val){
+            return lowestCommonAncestor(root -> left, p, q);
         }
 
-        TreeNode* leftLCA = lowestCommonAncestor(root -> left, p, q);
-        TreeNode* rightLCA = lowestCommonAncestor(root -> right, p, q);
-        
-        //CASE 4: leftLCA = not NULL(valid) and righLCA = not NULL(valid); return root
-
-        //becase both p and q have returned a not NULL value to the same node (first lowest
-        //ancestor encountered, same to be returned)
-        if(leftLCA != NULL && rightLCA != NULL){
-            return root;
+        if(p -> val > root -> val && q -> val > root -> val){
+            return lowestCommonAncestor(root -> right, p , q);
         }
 
-        //CASE 2:leftLCA = not NULL(valid) and righLCA = NULL(not valid); return leftLCA
-        //because the other one is NULL 
-        else if(leftLCA != NULL){
-            return leftLCA;
-        }
-
-        //CASE 3:leftLCA = NULL(not valid) and righLCA = not NULL(valid); return rightLCA
-        //because the other one is NULL
-        else{
-            return rightLCA;
-        }
+        return root;
     }
 };
