2ba8989a4228aaaa166c920ab8e3bcfc88920058
ars231106 <aryansurani32@gmail.com>
2026-08-15 16:34:37 +0530

    Time: 1 ms (42.69%), Space: 12.6 MB (24.99%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -17,21 +17,46 @@ public:
         return ans;  */
 
 //dfs - backtracking approach
-    vector<vector<int>> ans;
+
+ //   vector<vector<int>> ans;
     
-    void dfsBacktrack(vector<int>& nums, vector<int>& curr, int i){
-        ans.push_back(curr);
+     /*void dfsBacktrack(vector<int>& nums, vector<int>& curr, int i){
+       ans.push_back(curr);
 
         for(int j = i; j<nums.size(); j++){
             curr.push_back(nums[j]);
             dfsBacktrack(nums, curr, j + 1);
             curr.pop_back();
         }
+    } */
+
+//binary/ (choose/not choose) backtracking approach 
+
+    void subsequence(vector<int>& nums, int index, int n, vector<vector<int>>& ans, vector<int>& temp){
+        if(index == n){
+            ans.push_back(temp);
+            return;
+        }
+
+        subsequence(nums, index + 1, n, ans, temp);
+        temp.push_back(nums[index]);
+        subsequence(nums, index + 1, n, ans, temp);
+        temp.pop_back();
     }
 
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<int> curr;    
+       /* vector<int> curr;    
         dfsBacktrack(nums, curr, 0);
+        return ans;  
+        
+        (for for loop (dfs) backtarcking approach)
+        
+        */
+
+        vector<vector<int>> ans;
+        vector<int> temp;
+        subsequence(nums, 0, nums.size(), ans, temp);
+
         return ans;
-    }
+    } 
 };
