3389b7ec66cfd68393c41c701b68f9a22d59d4a3
ars231106 <aryansurani32@gmail.com>
2026-09-01 10:31:53 +0530

    Time: 0 ms (100%), Space: 23 MB (55.44%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-    pair<int,int> dfs(TreeNode* root) {
-        if(root == NULL) {
-            return {0, 1};
+    int dfs(TreeNode* root){
+        if (root == NULL){
+            return 0;
         }
 
-        pair<int,int> left = dfs(root->left);
-        pair<int,int> right = dfs(root->right);
+        int leftside = dfs(root -> left);
+        if(leftside == -1){
+            return -1;
+        }
 
-        int height = 1 + max(left.first, right.first);
+        int rightside = dfs(root -> right);
+        if(rightside == -1){
+            return -1;
+        }
 
-        int balanced = left.second &&
-                       right.second &&
-                       abs(left.first - right.first) <= 1;
+        if(abs(leftside - rightside) > 1){
+            return -1;
+        }
 
-        return {height, balanced};
+        return 1 + max(leftside, rightside);
     }
-
     bool isBalanced(TreeNode* root) {
-        pair<int,int> ans = dfs(root);
-
-        return ans.second;
+        return dfs(root) != -1;
     }
 };
